28879d8ce11ef11698aa35b8c04031060937b755
victoriasantacruzbyu <victoriastc21@gmail.com>
2026-05-09 11:31:00 -0300

    week1

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+document.querySelector("#currentyear").textContent =
+new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+`Last Modification: ${document.lastModified}`;
